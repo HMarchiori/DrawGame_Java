619fc0193d4608f41ccbb6e0f22de11a6d155958
HMarchiori <hmarchiori2003@gmail.com>
2023-12-11 21:26:29 -0300

    added comments

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        // Creates an instance of Application and calls the method that starts the game
         Application application = new Application();
+        // Insert the names of the players and the number of cards they will have
         application.insertNameCards();
+        // Creates the cards for each player
         application.createCards();
+        // Starts the game
         application.draw();
+        // Prints the information of the winners
         application.printInformation();
     }
 }
